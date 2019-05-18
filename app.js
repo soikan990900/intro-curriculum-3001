1a351b43817a3900a074aba9f32e8e0ca494af8f
soikan990900 <soikan990900@gmail.com>
2019-05-18 20:18:31 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 * 与えられた自然数の階乗を返す
 * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
 * @param {Number} n
-* @returns {Number}
+* @returns {Number} 
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (let i = 1; i <= n; i++){
+    result = result * i;
+    }
+
     return result;
 }
 const assert = require('assert');
